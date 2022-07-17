d9a630feb23df0476abb25b96ae36baee77e2499
Paweł Stolecki <pawel.stolecki@techland.pl>
2022-07-17 15:50:40 +0200

    premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Dream"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Dream"
+    location "Dream"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "DM_PLATFORM_WINDOWS",
+            "DM_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "DM_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "DM_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "DM_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Dream/vendor/spdlog/include",
+        "Dream/src"
+    }
+
+    links
+    {
+        "Dream"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "DM_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "DM_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "DM_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "DM_DIST"
+        optimize "On"
